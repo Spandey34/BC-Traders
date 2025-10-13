4682b61fa5e35d3b2a355f44202ea76b14331c29
Spandey34 <shubhamkumarpandey1844@gmail.com>
2025-10-14 04:45:17 +0530

    cors issue started

--- server/index.js
+++ server/index.js
@@ -11,11 +11,13 @@ import adminMiddleware from "./middlewares/adminMiddleware.js";
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 connectDB();
